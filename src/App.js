f88609688698f8c90cfbf40e391cd5407e4a712a
alaa tohamy <alaa.tohamy@robustastudio.com>
2019-09-28 04:49:00 +0200

    Feat: Add counter and 2 buttons to increase and decrease this counter using apollo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from './components/Counter';
+import Increment from './components/Increment';
+import Decrement from './components/Decrement';
 
 class App extends Component {
   render() {
@@ -11,14 +14,9 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Counter />
+          <Increment />
+          <Decrement />
         </header>
       </div>
     );
